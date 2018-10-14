5f073ceb2a2bc3f7ad6335a55d170eee91aa5cf7
Wen Tjun <wentjun289@hotmail.com>
2018-10-15 02:51:32 +0800

    loading notes with content
    - collapsible list item to display content

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const noteList = [
   {
     title: 'Instructions (click to expand)',
     content:
-      'Simply create a note by typing your title and clicking the add button!'
+      'Simply create a note by typing your title and clicking the add button! You may also edit your title or content by simply clicking on the title or content'
   },
   {
     title: 'Sample note',
@@ -23,6 +23,7 @@ const init = () => {
   for (var i = 0; i < noteList.length; i++) {
     renderNote(i, noteList[i].title, noteList[i].content);
   }
+  renderCollapsible();
 };
 
 // renders individual note element
@@ -39,7 +40,55 @@ const renderNote = (index, title, content) => {
   // appends title to note
   noteListItem.appendChild(noteListItemTitle);
 
+  // creates note content div
+  const noteListItemParagraph = document.createElement('div');
+  const noteListItemContent = document.createElement('p');
+  const contentNode = document.createTextNode(content);
+  noteListItemParagraph.className = 'content';
+  noteListItemContent.contentEditable = 'true';
+  noteListItemContent.setAttribute(
+    'data-placeholder',
+    'Click and type something..'
+  );
+  noteListItemContent.addEventListener(
+    'keyup',
+    event => {
+      noteListItemParagraph.style.maxHeight =
+        noteListItemParagraph.scrollHeight + 'px';
+    },
+    false
+  );
+  noteListItemContent.appendChild(contentNode);
+  noteListItemParagraph.appendChild(noteListItemContent);
+  // appends content to note
+  noteListItem.appendChild(noteListItemParagraph);
+
   document.getElementById('notes').appendChild(noteListItem);
+};
+
+// Allows the note div to collapse/uncollapse when clicked. Click to show content of notes.
+const renderCollapsible = () => {
+  const list = document.querySelector('ul');
+  list.addEventListener('click', showContent, false);
+};
+
+// allows user to expand/collapse by clicking the entire note div
+const showContent = event => {
+  if (event.target.tagName === 'DIV' && event.target.className !== 'content') {
+    const content = event.target.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  } else if (event.target.tagName === 'LI') {
+    const content = event.target.querySelector('div.content');
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  }
 };
 
 init();
